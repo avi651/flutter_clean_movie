001d001654810b4b4fed5052a09a3742ed72efb1
avi651 <kumaravi651@gmail.com>
2026-04-03 03:56:21 +0530

    Added the test case

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Basic test', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(home: Scaffold(body: Text('Test'))),
-    );
-
-    expect(find.text('Test'), findsOneWidget);
+  test('simple test', () {
+    expect(1 + 1, 2);
   });
 }
