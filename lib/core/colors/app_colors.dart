0d9552d04afb8df78485c3be5bc75eb4d8b38ce1
avi651 <kumaravi651@gmail.com>
2026-04-03 03:22:00 +0530

    WIP

--- lib/core/colors/app_colors.dart
+++ lib/core/colors/app_colors.dart
@@ -1,4 +1,3 @@
-import 'package:movie_clean/core/colors/interfaces/app_colors_inteface.dart';
 import 'package:movie_clean/core/colors/variations/dark/dark_app_colors.dart';
 import 'package:movie_clean/core/colors/variations/light/light_app_colors.dart';
 import 'package:movie_clean/core/colors/variations/light/vendor_app_colors.part.dart';
--- lib/domain/logger/log_level.dart
+++ lib/domain/logger/log_level.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 enum LogLevel {
   error,
   critical,
